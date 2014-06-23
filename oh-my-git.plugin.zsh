65fa42e2b16af109bb5072759aaaa397d48ac145
Brice Dutheil <brice.dutheil@gmail.com>
2014-06-23 19:34:50 +0200

    Adds documentation to the oh-my-git plugin

--- oh-my-git.plugin.zsh
+++ oh-my-git.plugin.zsh
@@ -1,7 +1,25 @@
 # Based on the work of fabulous work of Arialdo Martini
 # https://github.com/arialdomartini/oh-my-git/
 #
-# Code taken from : https://github.com/arialdomartini/oh-my-git/pull/22
+# This derivative fork is taken from my pull request on his project here : https://github.com/arialdomartini/oh-my-git/pull/22
+#
+# ABSTRACT
+# ========
+#
+# This new git information script (`oh-my-git.plugin.zsh`)
+#    * is not rely on a prompt shell anymore
+#    * is using configurable color codes
+#    * doesn't leak variables
+#    * does not depend on external configuration
+#    * offers configurable color codes per symbol
+#    * has a more understandable name, that can be easily used in another context (I'm using it in my own crafted oh-my-zsh theme, though it's named `oh-my-git.plugin.zsh`)
+#    * offers suffix and prefix config
+#    * enables toggling of the display of the git repository symbol (`display_git_symbol`)
+#    * enables to toggle if empty spaces for _off_ flags will be displayed (`use_color_off=false` and flag is off, toggable with `print_unactive_flags_space`)
+#    * enables toggling per git repository config `git config --get oh-my-zsh.hide-status`
+#    * displays current git action (`REBASE-i`, `REBASE-m`, `REBASE`, `AM/REBASE`, `MERGING`, `BISECTING`, `CHERRY-PICKING`)
+#
+
 
 function oh_my_git_info {
 	local oh_my_git_string="";
@@ -73,6 +91,7 @@ function oh_my_git_info {
 
 	# Early return if git repo is configured to be hidden
 	if [[ "$(git config --get oh-my-zsh.hide-status)" == "1" ]]; then return; fi
+	# if [[ "$(git config --get oh-my-git.enabled)" != "true" ]]; then return; fi
 
 	# Git info
 	local current_commit_hash=$(git rev-parse HEAD 2> /dev/null)
